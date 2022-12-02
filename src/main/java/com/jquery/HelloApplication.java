cb00d0bb1713e211bc726f6c808e6efa599e8845
Sonhr1403 <sonhr1403@gmail.com>
2022-12-02 08:44:20 +0700

    Initial commit

---
+++ src/main/java/com/jquery/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.jquery;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
